64ac86f9015f76dad8e2b2e05904c71c4b8221c5
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-11 18:39:50 +0800

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,28 @@
-import aboutImg from '../assets/img-1.jpg'
-import '../styles/About.css'
+import aboutImg from "../assets/img-1.jpg";
+import "../styles/About.css";
 
 const About = ({ isDarkMode }) => {
-    return (
-        <div className={`about-container ${isDarkMode ? "dark-mode" : ""}`}>
-            <div className="about-text">
-                <h1>About</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorum aperiam nam neque pariatur quis obcaecati porro, dignissimos nulla dicta
-                    excepturi odio vero aliquid vitae totam repudiandae dolore nihil animi! Recusandae expedita, illo quibusdam alias reprehenderit doloribus
-                    repellendus id autem non enim, dicta labore quidem est mollitia optio magni dolores!</p>
-            </div>
-            <div className="about-img">
-                <img src={aboutImg} className="about-image" style={{ borderRadius: "10px" }} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`about-container ${isDarkMode ? "dark-mode" : ""}`}>
+      <div className="about-text">
+        <h1>About</h1>
+        <p>
+          This Journal is About my Internship Experience during my stay at
+          Chanel Precision. The things I did and things that I have learned and
+          experienced so far staying there. Disclaimer I didn't include anything
+          that tackles about my tasks as a Intern Data Analyst in the company
+          because it is forbidden.
+        </p>
+      </div>
+      <div className="about-img">
+        <img
+          src={aboutImg}
+          className="about-image"
+          style={{ borderRadius: "10px" }}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -1,20 +1,28 @@
-import challengesImg from '../assets/img-2.jpeg'
-import '../styles/Challenges.css'
+import challengesImg from "../assets/img-2.jpeg";
+import "../styles/Challenges.css";
 
 const Challenges = ({ isDarkMode }) => {
-    return (
-        <div className={`hero-container ${isDarkMode ? "dark-mode" : ""}`}>
-            <div className="challenges-text">
-                <h1>Challenges</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolorum aperiam nam neque pariatur quis obcaecati porro, dignissimos nulla dicta
-                    excepturi odio vero aliquid vitae totam repudiandae dolore nihil animi! Recusandae expedita, illo quibusdam alias reprehenderit doloribus
-                    repellendus id autem non enim, dicta labore quidem est mollitia optio magni dolores!</p>
-            </div>
-            <div className="challenges-img">
-                <img src={challengesImg} className="challenges-image" style={{ borderRadius: "10px" }} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`hero-container ${isDarkMode ? "dark-mode" : ""}`}>
+      <div className="challenges-text">
+        <h1>Challenges</h1>
+        <p>
+          Challenge is an obstacle that an individual needs to overcome to be
+          successful, During my stay at Chanel. To be honest it wasn't easy at
+          all. The tasks is overwhelming, the adjustment that I needed to do
+          since I work at night. A lot of challenges will be covered in some of
+          my blogs in the collections slider.
+        </p>
+      </div>
+      <div className="challenges-img">
+        <img
+          src={challengesImg}
+          className="challenges-image"
+          style={{ borderRadius: "10px" }}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Challenges
+export default Challenges;
